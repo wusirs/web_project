404915f070795fc1f2aefe6741557d5d8badb304
wusirs <2411851860@qq.com>
2020-08-30 10:42:52 +0800

    冒泡排序算法修改

--- bubble.java
+++ bubble.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 public class bubble{
 	public static void main(String[] args){
 		int[] nums = {14,19,8,9,1,78,29,45,23};
-
-		 for(int i = 0; i < nums.length; i++){				                         System.out.print(nums[i]);
+		for(int i = 0; i < nums.length; i++){
+			System.out.print(nums[i] + ", ");
 		}
-
+		System.out.println();
 
 		for(int i=0; i<nums.length - 1; i++){
 			for(int j = 0; j < nums.length - i - 1; j ++){
@@ -19,7 +19,7 @@ public class bubble{
 		}
 
 		for(int i = 0; i < nums.length; i++){
-			System.out.print(nums[i]);
+			System.out.print(nums[i] + ", ");
 		}
 
 		System.out.println();
